629c63db19d96236681e09466fc032c41ef9c7d8
Jan Collijs <jan.collijs@inuits.eu>
2013-01-22 13:30:44 +0100

    Initial commit

---
+++ manifests/install.pp
@@ -0,0 +1,8 @@
+# Class: monitis::install
+#
+# Installation of the monitis package
+class monitis::install {
+  package { $::monitis::pkg_name :
+    ensure  => 'latest'
+  }
+}
---
+++ manifests/params.pp
@@ -0,0 +1,14 @@
+# Class monitis::params
+#
+# This class contains every parameter which will be used in this puppet module
+# for setting up the monitis agent
+class monitis::params {
+  $pkg_name     = 'monitis-agent'
+  $conffile     = '/etc/monitis/etc/monitis.conf'
+  $homedir      = '/etc/monitis'
+  $logfile      = '/etc/monitis/logs/monitis.log'
+  $pidfile      = '/etc/monitis/logs/monitis.pid'
+  $useremail    = undef
+  $agentname    = undef
+  $usehost      = 'https://174.37.16.80:443/fcgi-bin/agentgateway'
+}
