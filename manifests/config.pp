e3af2dd3e9fe24a9f568d4748a6742b52ba66bcd
Jan Collijs <jan.collijs@inuits.eu>
2013-04-08 15:39:45 +0200

    Added restart service after changed config file

--- manifests/config.pp
+++ manifests/config.pp
@@ -3,10 +3,12 @@
 # Class which configures the monitis agent service
 class monitis::config {
   file { $::monitis::conffile :
-    owner   => 'root',
-    group   => 'root',
-    replace => true,
-    content => template('monitis/monitis.conf.erb'),
+    owner        => 'root',
+    group        => 'root',
+    replace      => true,
+    content      => template('monitis/monitis.conf.erb'),
+    refresh_only => true,
+    notify       => Exec['restart monitis'];
   }
 
   exec { 'restart monitis':
